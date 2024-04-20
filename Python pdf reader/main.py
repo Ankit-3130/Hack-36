5d8f6d080c8959b3906ef7fdb76e4ec57308e936
cluelessmonk <neverstopwithme@gmail.com>
2024-04-20 20:59:25 +0530

    update2

--- Python pdf reader/main.py
+++ Python pdf reader/main.py
@@ -8,14 +8,15 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
-    "https://localhost:5173",
-     "http://localhost:5174",
     "https://localhost:5174",
+    "127.0.0.1/5173",
+    "127.0.0.1/5174",
+
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5174", "http://localhost:5173", "https://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
