7c28d85c27b4f9451a3a2479d62c6db711c3063e
Ayush-Kumar0 <ayushkumar27053@gmail.com>
2024-04-20 22:05:41 +0530

    changed auth

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/login/Login";
-import LoginPageStudent from "./pages/login/LoginStudent";
-import LoginPageInstitution from "./pages/login/LoginInstitution";
-
-import Register from "./pages/login/Register";
+import Home from "./pages/login/Home";
 import RegisterStudent from "./pages/login/RegisterStudent";
-
 import StudentDashboard from "./pages/studentDashboard";
 import AdminDashboard from "./pages/adminDashboard";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import Loader from "./components/loaders/loader";
 
@@ -17,12 +13,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Login */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/studentlogin" element={<LoginPageStudent />} />
-        <Route path="/institutionlogin" element={<LoginPageInstitution />} />
+        {/* Home */}
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/studentlogin" element={<LoginPageStudent />} /> */}
+        {/* <Route path="/institutionlogin" element={<LoginPageInstitution />} /> */}
         {/* Register */}
-        <Route path="/register" element={<Register />} />
+        {/* <Route path="/register" element={<Register />} /> */}
         <Route path="/studentregister" element={<RegisterStudent />} />
         {/* Dashboards */}
         <Route path="/studentdashboard" element={<StudentDashboard />} />
--- Client/src/pages/login/Home.jsx
+++ Client/src/pages/login/Home.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import bg from "../../images/loginbg.jpg";
 import { useNavigate } from "react-router-dom";
 import "./styles.css";
+import "./home.css";
 import Loader from "../../components/loaders/loader";
 
 
-const Login = () => {
+const Home = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
 
@@ -13,13 +14,20 @@ const Login = () => {
     setLoading(false);
   }, []);
 
+  const handleConnectToMetamask = () => {
+    // connect to metamask here
+    navigate('/studentregister');
+    // navigate('/studentdashboard');
+    // navigate('/admindashboard');
+  }
+
   return (
     <>
       {loading &&
         <Loader backgroundColor={'#000000ff'} />}
 
       <div className=" animate-gradient h-screen w-screen flex items-center justify-center border-black shadow-2xl ">
-        <div className="relative w-2/4 ml-72 mr-72 h-3/4 rounded-3xl">
+        <div className="relative home-card rounded-3xl">
 
           {/* Card background */}
           <div className="absolute top-0 bg-cover w-full h-full z-[0]">
@@ -34,39 +42,28 @@ const Login = () => {
             {/* Login Heading */}
             <div className="flex justify-center">
               <h1 className="text-white mb-20 font-bold text-5xl flex justify-center">
-                Login
+                Thalachain
               </h1>
             </div>
 
-            {/* Login page center */}
+            {/* Card center */}
             <div>
 
 
-              {/* Login as student button */}
+              {/* Connect to metamask button */}
               <button
-                className=" mb-10 bg-white flex rounded-xl font-bold w-40 justify-center h-10 items-center text-lg hover:scale-110 hover:bg-gray-400"
-                onClick={() => {
-                  navigate("/studentlogin");
+                className=" mb-10 bg-white flex rounded-xl font-bold p-2 justify-center items-center text-lg hover:scale-110 hover:bg-gray-400"
+                onClick={(e) => {
+                  handleConnectToMetamask()
                 }}
               >
-                As Student
+                Connect Metamask
               </button>
 
-
-
-              {/* Login as institution button */}
-              <button
-                className=" bg-white flex rounded-xl font-bold w-40 justify-center h-10 items-center text-lg hover:scale-110 hover:bg-gray-400"
-                onClick={() => {
-                  navigate("/institutionlogin");
-                }}
-              >
-                As Institution
-              </button>
 
 
               {/* Register page link */}
-              <button
+              {/* <button
                 className="text-white mt-24 ml-2 hover:underline"
                 onClick={() => {
                   navigate("/register");
@@ -74,7 +71,8 @@ const Login = () => {
               >
                 {" "}
                 new ? register here !{" "}
-              </button>
+              </button> */}
+
             </div>
 
 
@@ -88,4 +86,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Home;
