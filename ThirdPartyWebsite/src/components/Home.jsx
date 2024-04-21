8e13bedbd349a8e3b2e390664c3c89f678faad0e
Ayush-Kumar0 <ayushkumar27053@gmail.com>
2024-04-21 09:11:46 +0530

    updated

--- ThirdPartyWebsite/src/components/Home.jsx
+++ ThirdPartyWebsite/src/components/Home.jsx
@@ -1,30 +1,31 @@
 import React, { useEffect, useState } from 'react'
-import { ethers } from "ethers";
+// import { ethers } from "ethers";
 
 function Home() {
     const [formData, setFormData] = useState([{ subject: "", "subject code": "", marks: "" }]);
 
     const handleAutoFill = () => {
         // call an api
-        setFormData([{ subject: "maths", "subject code": "mat", marks: 84 },
-        { subject: "maths", "subject code": "mat", marks: 84 },
-        { subject: "maths", "subject code": "mat", marks: 84 },
+        setFormData([{ subject: "English", "subject code": "end", marks: 40 },
+        { subject: "Maths", "subject code": "mat", marks: 54 },
+        { subject: "Science", "subject code": "sci", marks: 42 },
+        { subject: "Hindi", "subject code": "hin", marks: 67 },
         ]);
     }
 
     const [address, setAddress] = useState();
     const [details, setDetails] = useState([]);
     const course = ["Class 10th", "Class 12th", "B.Tech"];
-    useEffect(() => {
-        const Request = async () => {
-            await window.ethereum.request({ method: 'eth_requestAccounts' });
-            const Web3provider = new ethers.providers.Web3Provider(window.ethereum);
-            const signer = Web3provider.getSigner();
-            const Address = await signer.getAddress();
-            setAddress(Address);
-        }
-        Request();
-    }, [])
+    // useEffect(() => {
+    //     const Request = async () => {
+    //         await window.ethereum.request({ method: 'eth_requestAccounts' });
+    //         const Web3provider = new ethers.providers.Web3Provider(window.ethereum);
+    //         const signer = Web3provider.getSigner();
+    //         const Address = await signer.getAddress();
+    //         setAddress(Address);
+    //     }
+    //     Request();
+    // }, [])
 
     const getDetails = async () => {
         fetch(`http://localhost:8080/getRecords/${address}`, {
